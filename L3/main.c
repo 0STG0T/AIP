42bec1038cfa4a217fe5bd9a09334ee3d62badfa
0STG0T <ostgot116@gmail.com>
2024-10-25 19:56:11 +0300

    L3

--- L3/main.c
+++ L3/main.c
@@ -1,43 +1,69 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "safe_input.c"
 
-#define N 10
+#define N 2 // Задаем размер матрицы
 
-// Function to initialize and input a matrix
-double** init_input_matrix() {
-    // Dynamically allocate memory for an N x N matrix
-    double** matrix = (double**) malloc(N * sizeof(double*));
-    for (int i = 0; i < N; i++) {
-        matrix[i] = (double*) malloc(N * sizeof(double));
-    }
 
-    // Read values into the matrix
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            scanf("%lf", &matrix[i][j]);  // Corrected format specifier for double
-        }
-    }
+// функция ввода матрицы
+void input_matrix(int matrix[N][N]);
 
-    return matrix;
+// функция вывода исходной матрицы
+void print_matrix(int matrix[N][N]);
+
+// функция вывода матрицы в заданном порядке
+void print_matrix_in_order(int matrix[N][N]);
+
+
+int main(){
+    // инициализация матрицы
+    int matrix[N][N];
+
+    // ввод матрицы
+    input_matrix(matrix);
+
+    // вывод заданной матрицы
+    print_matrix(matrix);
+
+    // вывод матрицы в заданном порядке
+    print_matrix_in_order(matrix);
 }
 
-int main() {
-    // Get the input matrix
-    double** matrix = init_input_matrix();
+void input_matrix(int matrix[N][N]){
+    printf("Приступим к вводу квадратной матрицы порядка %d.\n", N);
+    for (int i=0;i<N;i++){
+        for (int j=0;j<N;j++){
+            printf("Введите элемент матрицы [%d, %d]: ", i, j);
+            matrix[i][j] = safe_input_int("");
+        }
+    }
+    printf("\n");
+}
 
-    // Print the matrix
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            printf("%lf ", matrix[i][j]);
+void print_matrix(int matrix[N][N]){
+    printf("Введенная вами матрица:\n");
+    for (int i=0;i<N;i++){
+        for (int j=0;j<N;j++){
+            printf("%d ", matrix[i][j]);
         }
         printf("\n");
     }
+    printf("\n");
+}
 
-    // Free the dynamically allocated memory
-    for (int i = 0; i < N; i++) {
-        free(matrix[i]);
+void print_matrix_in_order(int matrix[N][N]){
+    printf("Последовательный вывод матрицы в указанном порядке:\n");
+    // выводим первый элемент последовательности
+    printf("%d\n", matrix[N-1][0]);
+    // я выбрал идти по главной диагонали снизу вверх
+    for (int i=N-2;i>=0;i--){
+        // вывод горизонтальной части угла слева на право, включая вершину угла
+        for (int j=0;j<=(N-1 - i);j++){
+            printf("%d ", matrix[i][j]);
+        }
+        // вывод вертикальной части угла сверху вниз, не включая вершину угла
+        for (int j=i+1;j<N;j++){
+            printf("%d ", matrix[j][N-1 - i]);
+        }
+        printf("\n");
     }
-    free(matrix);
-
-    return 0;
 }
